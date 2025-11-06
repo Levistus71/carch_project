069622a4ec4d5048f7900c28d0e3ba31ac35fc33
Srinivas Reddy <srinivasreddyguntaka71@gmail.com>
2025-11-06 22:25:57 +0530

    forgot checking tag in btb lol. added it

--- src/vm/rv5s/hardware/branch_predictor.cpp
+++ src/vm/rv5s/hardware/branch_predictor.cpp
@@ -10,6 +10,8 @@ std::pair<bool, uint64_t> BranchPredictor::static_predict(uint64_t branch_instru
 
     if(!btb_value.valid) return {false, branch_instruction_address + 4};
 
+    if(branch_instruction_address != btb_value.tag) return {false, branch_instruction_address + 4};
+
     // target_address is less than current address, branch backward (loops)
     if(btb_value.target_address < branch_instruction_address){
         return {true, btb_value.target_address};
@@ -25,6 +27,8 @@ std::pair<bool, uint64_t> BranchPredictor::dynamic_predict(uint64_t branch_instr
 
     if(!btb_value.valid) return {false, branch_instruction_address + 4};
 
+    if(branch_instruction_address != btb_value.tag) return {false, branch_instruction_address + 4};
+
     if(btb_value.take_branch){
         return {true, btb_value.target_address};
     }
@@ -38,12 +42,10 @@ void BranchPredictor::update_btb(uint64_t branch_instruction_address, bool branc
     uint8_t index = (branch_instruction_address >> sll_pc) & btb_index_mask;
     BTBValue& btb_value = branch_target_buffer[index];
 
-    if(!btb_value.valid){
-        btb_value.valid = true;
-    }
-
+    btb_value.valid = true;
     btb_value.take_branch = branch_taken;
     btb_value.target_address = target_address;
+    btb_value.tag = branch_instruction_address;
 }
 
 void BranchPredictor::reset(){
