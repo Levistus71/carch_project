2c595cf470a011cbf839791ebe726e70336135e2
Srinivas Reddy <srinivasreddyguntaka71@gmail.com>
2025-11-24 20:28:10 +0530

    bug.

--- src/vm/rv5s/pipelined/hardware/hazard_detector.cpp
+++ src/vm/rv5s/pipelined/hardware/hazard_detector.cpp
@@ -227,7 +227,7 @@ void HazardDetector::HandleControlHazard(PipelinedCore& vm_core){
     PipelinedInstrContext& ex_instruction = vm_core.GetExInstruction();
 
     if(vm_core.branch_prediction_enabled_ && !ex_instruction.branch_jalr){
-        vm_core.branch_predictor_.update_btb(ex_instruction.pc, ex_instruction.branch_taken, vm_core.program_counter_);
+        vm_core.branch_predictor_.update_btb(ex_instruction.pc, ex_instruction.branch_taken, ex_instruction.pc + ex_instruction.immediate);
     }
     
     PipelinedInstrContext& if_instruction = vm_core.GetIfInstruction();
